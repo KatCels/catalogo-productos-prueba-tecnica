e26b802d2e443be2885a43136e8f32e2ab1dedf0
Kat Cels <katherinecamacho12@gmail.com>
2025-06-28 16:38:15 -0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,2 +1,39 @@
 // Lista de productos de ejemplo
-const productos = [];
+
+const products = [
+  {
+    id: 1,
+    name: "Camiseta básica",
+    price: 25,
+    category: "Ropa",
+    image: "assets/images/camiseta.jpg"
+  },
+  {
+    id: 2,
+    name: "Zapatillas deportivas",
+    price: 80,
+    category: "Calzado",
+    image: "assets/images/zapatillas.jpg"
+  },
+  {
+    id: 3,
+    name: "Gorra ajustable",
+    price: 15,
+    category: "Accesorios",
+    image: "assets/images/gorra.jpg"
+  },
+  {
+    id: 4,
+    name: "Pantalón jeans",
+    price: 45,
+    category: "Ropa",
+    image: "assets/images/pantalon.jpg"
+  },
+  {
+    id: 5,
+    name: "Botas de cuero",
+    price: 100,
+    category: "Calzado",
+    image: "assets/images/botas.jpg"
+  }
+];
