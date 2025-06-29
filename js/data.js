ca88bbae6bf0fcfd37d96497ef3875ff547ccb7a
Kat Cels <katherinecamacho12@gmail.com>
2025-06-28 19:12:49 -0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -6,34 +6,34 @@ const products = [
     name: "Camiseta básica",
     price: 25,
     category: "Ropa",
-    image: "assets/imágenes/camiseta.jpg"
+    image: "assets/imágenes/camiseta-medellin.jpg"
   },
   {
     id: 2,
     name: "Zapatillas deportivas",
     price: 80,
     category: "Calzado",
-    image: "assets/imágenes/zapatillas.jpg"
+    image: "assets/imágenes/zapatillas-deportivas.png"
   },
   {
     id: 3,
     name: "Gorra ajustable",
     price: 15,
     category: "Accesorios",
-    image: "assets/imágenes/gorra.jpg"
+    image: "assets/imágenes/gorra-vans.jpg"
   },
   {
     id: 4,
     name: "Pantalón jeans",
     price: 45,
     category: "Ropa",
-    image: "assets/imágenes/pantalon.jpg"
+    image: "assets/imágenes/pantalon-jeans.jpg"
   },
   {
     id: 5,
     name: "Botas de cuero",
     price: 100,
     category: "Calzado",
-    image: "assets/imágenes/botas.jpg"
+    image: "assets/imágenes/botas-cuero.jpg"
   }
 ];
